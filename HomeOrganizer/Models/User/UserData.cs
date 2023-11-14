6f50152cdfd71668c2e50a49719d102312e86c84
ovrael <ovrael@gmail.com>
2023-11-14 17:51:41 +0100

    Added simply login placeholder

--- HomeOrganizer/Models/User/UserData.cs
+++ HomeOrganizer/Models/User/UserData.cs
@@ -5,13 +5,19 @@ namespace HomeOrganizer.Models.User
 {
     public class UserData
     {
-        public string Name { get; private set; } = "Admin";
+        public string Name { get; private set; } = "";
         public bool UseDarkTheme { get; private set; } = true;
 
         public List<IFeatureTileData> Features { get; private set; }
 
         public UserData()
         {
+            Features = FeaturesList.CreateNewFeatures();
+        }
+
+        public UserData(string name)
+        {
+            Name = name;
             Features = FeaturesList.CreateNewFeatures();
         }
     }
