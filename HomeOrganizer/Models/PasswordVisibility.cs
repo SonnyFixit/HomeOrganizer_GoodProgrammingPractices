0342f5771d481e2b6822f8705478b36636b31802
ovrael <ovrael@gmail.com>
2023-11-26 12:47:19 +0100

    Added password change feature to user panel

---
+++ HomeOrganizer/Models/PasswordVisibility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MudBlazor;
+
+namespace HomeOrganizer.Models
+{
+    public class PasswordVisibility
+    {
+        public bool ShowPassword { get; set; } = false;
+        public InputType InputType { get; set; } = InputType.Password;
+        public string Icon { get; set; } = Icons.Material.Filled.VisibilityOff;
+
+        public void ChangeVisibility()
+        {
+            ShowPassword = !ShowPassword;
+            if (ShowPassword)
+            {
+                InputType = InputType.Text;
+                Icon = Icons.Material.Filled.Visibility;
+            }
+            else
+            {
+                InputType = InputType.Password;
+                Icon = Icons.Material.Filled.VisibilityOff;
+            }
+        }
+    }
+}
--- HomeOrganizer/Services/EmailService.cs
+++ HomeOrganizer/Services/EmailService.cs
@@ -33,7 +33,7 @@ namespace HomeOrganizer.Services
                 "<head>" +
                 "<meta charset=\"UTF-8\">" +
                 "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" +
-                "<title>Resetowanie hasła</title>" +
+                "<title>Password reset</title>" +
                 "<style>  " +
                 "body {" +
                 "font-family: Arial, sans-serif;" +
@@ -67,19 +67,74 @@ namespace HomeOrganizer.Services
                 "</head>" +
                     "<body>" +
                         "<div class=\"email-container\">" +
-                        "<h2>Resetowanie hasła</h2>" +
-                        $"<p>Witaj, {login}</p>" +
-                        "<p>Otrzymaliśmy prośbę o zresetowanie hasła dla Twojego konta Home Organizer.</p>" +
-                        "<p>Jeśli to byłeś Ty, kliknij poniższy link, aby przejść do strony resetowania hasła:</p>" +
-                        $"<a class=\"button\" href=\"{resetUrl}\">Zresetuj hasło</a>" +
-                        "<p>Jeśli to nie byłeś Ty, zignoruj tę wiadomość.</p> " +
-                        "<p>Dziękujemy!</p>" +
-                        "<p>Zespół Home Organizer</p>" +
+                        "<h2>Password reset</h2>" +
+                        $"<p>Hello, {login}</p>" +
+                        "<p>We have received a password reset request for your Home Organizer account.</p>" +
+                        "<p>If this was you, click the link below to go to the password reset page:</p>" +
+                        $"<a class=\"button\" href=\"{resetUrl}\">Reset password</a>" +
+                        "<p>If this wasn't you, please contact us as fast as possible.</p> " +
+                        "<p>Thank you!</p>" +
+                        "<p>Home Organizer Team</p>" +
                         "</div>" +
                     "</body>" +
                 "</html>";
 
             await SendEmail(email, login, "Password reset", body);
+        }
+
+        public static async Task SendChangedPasswordNotification(string email, string login)
+        {
+            string body = "<!DOCTYPE html>" +
+                "<html lang=\"pl\">" +
+                "<head>" +
+                "<meta charset=\"UTF-8\">" +
+                "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" +
+                "<title>Password changed</title>" +
+                "<style>  " +
+                "body {" +
+                "font-family: Arial, sans-serif;" +
+                "background-color: #f2f2f2;" +
+                "}" +
+                ".email-container {" +
+                "max-width: 600px;" +
+                "margin: 0 auto;" +
+                "padding: 20px;" +
+                "background-color: #ffffff;" +
+                "border: 1px solid #dddddd;    }" +
+                "h2 {" +
+                "color: #333333;" +
+                "}" +
+                "p {" +
+                "color: #666666;" +
+                "margin-bottom: 20px;" +
+                "}" +
+                ".button {" +
+                "display: inline-block;" +
+                "padding: 10px 20px;" +
+                "background-color: #FFEAEA;" +
+                "color: #000000;" +
+                "text-decoration: none;" +
+                "border-radius: 3px;" +
+                "}" +
+                "a, a:link, a:visited, a:hover, a:active {" +
+                "color: #000000;" +
+                "}" +
+                "</style>" +
+                "</head>" +
+                    "<body>" +
+                        "<div class=\"email-container\">" +
+                        "<h2>Password changed!</h2>" +
+                        $"<p>Hello, {login}</p>" +
+                        "<p> Your password has been changed recently. </p>" +
+                        "<p>If this was you, you can ignore this message.</p>" +
+                        "<p>Otherwise please contact us as fast as possible.</p> " +
+                        "<p>Thank you!</p>" +
+                        "<p>Home Organizer Team</p>" +
+                        "</div>" +
+                    "</body>" +
+                "</html>";
+
+            await SendEmail(email, login, "Password changed", body);
         }
     }
 }
