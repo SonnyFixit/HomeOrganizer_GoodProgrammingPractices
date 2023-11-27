62c165326cc357b427deca73bf179b01bd6f4b77
ovrael <ovrael@gmail.com>
2023-11-27 10:10:00 +0100

    Fixed bug when user panel navigation change side reseted current open panel (to default -details)

---
+++ HomeOrganizer/Models/UserPanelNavigationData.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeOrganizer.Models
+{
+    public struct UserPanelNavigationData
+    {
+        public string Text;
+        public object Value;
+
+        public UserPanelNavigationData(string text, object value)
+        {
+            Text = text;
+            Value = value;
+        }
+    }
+}
